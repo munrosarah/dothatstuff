67c95e5884f2295a292e6019beec083e49032df2
SS Munro <munro.sarah@gmail.com>
2016-02-04 17:26:30 -0700

    remove logging statement

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,7 +6,6 @@ class List < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 100 }    
 
   def toggle_hidden!(toggle_value)  
-    Rails.logger.info("!!!!!!!!!!!!!!!!!!!#{toggle_value}")  
     self.hide_completed = 
       toggle_value.nil? ? !self.hide_completed : toggle_value
     self.save              
